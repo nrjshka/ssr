dbd6f0d3dac86afb06585a07f0a86bf2c43af2d3
nrjshka <nrjshka@gmail.com>
2018-09-15 18:17:14 +0300

    Added webpack config file

---
+++ server/webpack.server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+modules.exports = {
+  // Inform webpack that we are building bundle.js for NodeJs, rather than for the browser
+  target: 'node',
+
+  // Tell webpack the root file of our server application
+  entry: './src/index.js',
+
+  // Tell webpack where to put and output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+
+  // Tell babel to run all files it runs through
+  modules: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react', 
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] }} ]
+          ]
+        }
+      }
+    ]
+  }
+};
